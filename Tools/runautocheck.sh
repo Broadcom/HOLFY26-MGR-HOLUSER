d877b318acc738c91582721d652752decc1b63ec
bc004644 <bill.call@broadcom.com>
2025-06-10 08:34:04 -0500

    updating runautocheck.sh to check for autocheck2 if available and use autocheck public GitHub repo if not

--- Tools/runautocheck.sh
+++ Tools/runautocheck.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
-# version 1.4 09-May 2025
+# version 1.5 10-June 2025
 
 if [ ! -f ~holuser/rtrcreds.txt ]; then
    echo "Enter the password for the holorouter:"
@@ -15,7 +15,20 @@ autocheckdir=~holuser/autocheck
 [ -d $autocheckdir ] && rm -rf $autocheckdir
 
 echo "Cloning AutoCheck from public GitHub..."
-git clone -b main https://github.com/broadcom/HOLFY26-MGR-AUTOCHECK.git $autocheckdir
+
+jcount=0
+autorepo="https://github.com/broadcom/HOLFY26-MGR-AUTOCHECK2.git"
+git ls-remote ${autorepo} -q > /dev/null 2>&1 &
+jcount=$(jobs | wc -l)
+if [ ${jcount} != 0 ] ; then
+   echo "${autorepo} repo does not exist."
+   kill %1
+else
+   autorepo="https://github.com/broadcom/HOLFY26-MGR-AUTOCHECK.git"
+   echo "AUTOCHECK2 repo does not exist yet. Using ${autorepo} instead."
+fi
+
+git clone -b main ${autorepo} $autocheckdir > /dev/null 2>&1
 
 # need to turn off proxyfiltering to install PSSQLite
 ~holuser/hol/Tools/proxyfilteroff.sh
