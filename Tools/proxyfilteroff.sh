1a8bb20f7c3b2937c458b98bea29f02318707c63
Burke Azbill <burkeazbill@users.noreply.github.com>
2025-07-17 08:41:59 -0400

    Comment cleanup and add VCF9-SSAUTO-WKLDSV

--- Tools/proxyfilteroff.sh
+++ Tools/proxyfilteroff.sh
@@ -5,7 +5,7 @@
 # removing support for pfSense router
 #if `nc -z 10.0.0.1 443`;then
 #	echo "pfSense router detected. Please remove the dot from the Squid denylist to open proxy."
-#	echo "Open https://10.0.0.1/-->Login:  admin, Password:  VMware1!-->Services-->Squid Proxy Server-->ACLs-->Denylist-->Delete the "dot"-->Save"
+#	echo "Open https://10.0.0.1/-->Login:  admin, Password:  ****** -->Services-->Squid Proxy Server-->ACLs-->Denylist-->Delete the "dot"-->Save"
 #	exit
 #fi
 
