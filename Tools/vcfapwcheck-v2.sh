a711598eb98f5f97c207cbadd750314e9662f712
Burke Azbill <burkeazbill@users.noreply.github.com>
2025-11-07 14:08:12 -0500

    revert to old version for further testing

---
+++ Tools/vcfapwcheck-v2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Author: Burke Azbill
+# Version: 2.0
+# Date: November, 2025
+# Purpose: Check VCF Automation appliance to see if the password has expired. If so, use expect script to reset the password
+
+function retry {
+    # Function source: https://serverfault.com/questions/80862/bash-script-repeat-command-if-it-returns-an-error
+    command="$*"
+    retval=1
+    attempt=1
+    until [[ $retval -eq 0 ]] || [[ $attempt -gt 6 ]]; do
+        # Execute inside of a subshell in case parent script is running with "set -e"
+        (
+            set +e
+            $command
+        )
+        retval=$?
+        (( attempt ++ ))
+        if [[ $retval -ne 0 ]]; then
+            # If there was an error wait 20 seconds
+            echo "Error when attempting to check for expired pw. Waiting 20s and then trying again..."
+            sleep 20
+        fi
+    done
+    if [[ $retval -ne 0 ]] && [[ $attempt -gt 6 ]]; then
+        # Something is fubar, go ahead and exit
+        echo "Error when attempting to check for expired pw. Tried for 3m, something is very wrong, failing..."
+        exit $retval
+    fi
+}
+
+# By the time this script is called, Automation should be accessible via SSH
+retry /home/holuser/hol/Tools/vcfapass.sh $(cat /home/holuser/creds.txt) $(/home/holuser/hol/Tools/holpwgen.sh)
