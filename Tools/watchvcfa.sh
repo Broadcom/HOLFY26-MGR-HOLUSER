756efc3249af12ef97bd085cfbb9cccda6e4817d
Burke Azbill <burkeazbill@users.noreply.github.com>
2025-11-20 16:55:12 -0500

    Fix date format to match python date in log output

--- Tools/watchvcfa.sh
+++ Tools/watchvcfa.sh
@@ -8,31 +8,31 @@ KUBESCHEDULER="."
 LOGFILE="/home/holuser/hol/labstartup.log"
 
 # Now try to remediate Automation
-echo "$(date +%F" "%T) -------------WATCHVCFA RUN START-------------" >> "${LOGFILE}"
-echo "$(date +%F" "%T)-> VCFA Watcher started"  >> "${LOGFILE}"
+echo "$(date +"%m/%d/%Y %T") -------------WATCHVCFA RUN START-------------" >> "${LOGFILE}"
+echo "$(date +"%m/%d/%Y %T")-> VCFA Watcher started"  >> "${LOGFILE}"
 
 # while [[ $REBOOTS -lt 3 || "$POSTGRES" != "2/2" ]]; do
   CNT=0
   while ! $(sshpass -f /home/holuser/creds.txt ssh -q -o ConnectTimeout=5 "vmware-system-user@10.1.1.71" exit); do
     sleep 30
-	  echo "$(date +%F" "%T)-> Waiting for VCFA to come Online" >> "${LOGFILE}"
+	  echo "$(date +"%m/%d/%Y %T")-> Waiting for VCFA to come Online" >> "${LOGFILE}"
     ((CNT++))
     if [ $CNT -eq 10 ]; then
-      echo "$(date +%F" "%T)-> VCFA Online check check tried 10 times (5m), continuing..." >> "${LOGFILE}"
+      echo "$(date +"%m/%d/%Y %T")-> VCFA Online check check tried 10 times (5m), continuing..." >> "${LOGFILE}"
       break
     fi
   done
-  # echo "$(date +%F" "%T)-> VCFA online, reboot# $REBOOTS" >> "${LOGFILE}" 
+  # echo "$(date +"%m/%d/%Y %T")-> VCFA online, reboot# $REBOOTS" >> "${LOGFILE}" 
 
   ###### Containerd check/fix ######
-  echo "$(date +%F" "%T)-> Checking containerd on VCFA for Ready,SchedulingDisabled..." >> "${LOGFILE}"
+  echo "$(date +"%m/%d/%Y %T")-> Checking containerd on VCFA for Ready,SchedulingDisabled..." >> "${LOGFILE}"
   CNT=0
   while [[ "$CONATAINERDREADY" != "" ]]; do
     ((CNT++))
     CONATAINERDREADY=$(sshpass -f /home/holuser/creds.txt ssh vmware-system-user@10.1.1.71 "sudo -i bash -c 'kubectl -s https://10.1.1.71:6443 get nodes '" | grep "Ready,SchedulingDisabled" | awk '{print $2}')
    
     if [ "$CONATAINERDREADY" == "Ready,SchedulingDisabled" ]; then
-      echo "$(date +%F" "%T)-> Stale containerd found, restarting..." >> "${LOGFILE}"
+      echo "$(date +"%m/%d/%Y %T")-> Stale containerd found, restarting..." >> "${LOGFILE}"
       sshpass -f /home/holuser/creds.txt ssh vmware-system-user@10.1.1.71 "sudo -i bash -c 'systemctl restart containerd'" >> "${LOGFILE}"
       sleep 5
       sshpass -f /home/holuser/creds.txt ssh vmware-system-user@10.1.1.71 "sudo -i bash -c 'kubectl -s https://10.1.1.71:6443 get nodes'" >> "${LOGFILE}"
@@ -43,13 +43,13 @@ echo "$(date +%F" "%T)-> VCFA Watcher started"  >> "${LOGFILE}"
       sshpass -f /home/holuser/creds.txt ssh vmware-system-user@10.1.1.71 "sudo -i bash -c 'kubectl -s https://10.1.1.71:6443 uncordon ${NODENAME}'"
     fi
     if [ $CNT -eq 3 ]; then
-      echo "$(date +%F" "%T)-> containerd check tried 3 times, continuing..." >> "${LOGFILE}"
+      echo "$(date +"%m/%d/%Y %T")-> containerd check tried 3 times, continuing..." >> "${LOGFILE}"
       break
     fi
   done
 
 ###### kube-scheduler check/fix ######
-  echo "$(date +%F" "%T)-> Checking kube-scheduler on VCFA for 0/1 Running..." >> "${LOGFILE}"
+  echo "$(date +"%m/%d/%Y %T")-> Checking kube-scheduler on VCFA for 0/1 Running..." >> "${LOGFILE}"
   CNT=0
   while [[ "$KUBESCHEDULER" != "" ]]; do
     ((CNT++))
@@ -57,13 +57,13 @@ echo "$(date +%F" "%T)-> VCFA Watcher started"  >> "${LOGFILE}"
     KUBESCHEDULER=$(sshpass -f /home/holuser/creds.txt ssh vmware-system-user@10.1.1.71 "sudo -i bash -c 'kubectl -n kube-system -s https://10.1.1.71:6443 get pods '" | grep "kube-scheduler" | grep "0/1" | awk '{print $2}')
    
     if [ "$KUBESCHEDULER" == "0/1" ]; then
-      echo "$(date +%F" "%T)-> Stale kube-scheduler found, restarting..." >> "${LOGFILE}"
+      echo "$(date +"%m/%d/%Y %T")-> Stale kube-scheduler found, restarting..." >> "${LOGFILE}"
       sshpass -f /home/holuser/creds.txt ssh vmware-system-user@10.1.1.71 "sudo -i bash -c 'systemctl restart containerd'" >> "${LOGFILE}"
       sleep 120
       sshpass -f /home/holuser/creds.txt ssh vmware-system-user@10.1.1.71 "sudo -i bash -c 'kubectl -n kube-system -s https://10.1.1.71:6443 get pods'" | grep "kube-scheduler" >> "${LOGFILE}"
     fi
     if [ $CNT -eq 3 ]; then
-      echo "$(date +%F" "%T)-> kube-scheduler check tried 3 times, continuing..." >> "${LOGFILE}"
+      echo "$(date +"%m/%d/%Y %T")-> kube-scheduler check tried 3 times, continuing..." >> "${LOGFILE}"
       break
     fi
   done
@@ -72,7 +72,7 @@ echo "$(date +%F" "%T)-> VCFA Watcher started"  >> "${LOGFILE}"
   #   cleaned up properly, resulting in the prevention of many other pods failint go start.
   #  Check this pod and delete it if it is old:
   # Delete the seaweedfs-master-0 pod if age over 1 hour
-  echo "$(date +%F" "%T)-> Checking seaweedfs-master-0 pod from VCFA if older than 1 hour..." >> "${LOGFILE}"
+  echo "$(date +"%m/%d/%Y %T")-> Checking seaweedfs-master-0 pod from VCFA if older than 1 hour..." >> "${LOGFILE}"
   CNT=0
   while [[ "$SEAWEEDPOD" != "" ]]; do
     ((CNT++))
@@ -80,14 +80,14 @@ echo "$(date +%F" "%T)-> VCFA Watcher started"  >> "${LOGFILE}"
     jq -r '. | select(.metadata.creationTimestamp | fromdateiso8601 < (now - 3600)) | .metadata.name ')
     
     if [ "$SEAWEEDPOD" == "seaweedfs-master-0" ]; then
-      echo "$(date +%F" "%T)-> Stale seaweedfs-master-0 pod found, deleting..." >> "${LOGFILE}"
+      echo "$(date +"%m/%d/%Y %T")-> Stale seaweedfs-master-0 pod found, deleting..." >> "${LOGFILE}"
       sshpass -f /home/holuser/creds.txt ssh vmware-system-user@10.1.1.71 "sudo -i bash -c 'kubectl -n vmsp-platform -s https://10.1.1.71:6443 delete pod seaweedfs-master-0'" >> "${LOGFILE}"
       sleep 5
       sshpass -f /home/holuser/creds.txt ssh vmware-system-user@10.1.1.71 "sudo -i bash -c 'kubectl -n vmsp-platform -s https://10.1.1.71:6443 get pods | grep seaweedfs'" >> "${LOGFILE}"
     fi
     sleep 5
     if [ $CNT -eq 3 ]; then
-      echo "$(date +%F" "%T)-> seaweedfs-master-0 check tried 3 times, continuing..." >> "${LOGFILE}"
+      echo "$(date +"%m/%d/%Y %T")-> seaweedfs-master-0 check tried 3 times, continuing..." >> "${LOGFILE}"
       break
     fi
   done
@@ -98,9 +98,9 @@ echo "$(date +%F" "%T)-> VCFA Watcher started"  >> "${LOGFILE}"
   #   sleep 60;
 	#   POSTGRES=$(sshpass -f /home/holuser/creds.txt ssh vmware-system-user@10.1.1.71 "sudo -i bash -c 'kubectl get pods -n prelude -s https://10.1.1.71:6443'" | grep vcfapostgres-0 | awk '{ print $2 }')
 	#   ((CNT++))
-  #   echo "$(date +%F" "%T)-> PG Running pods Result: $POSTGRES - Attempt: $CNT" >> "${LOGFILE}" 
+  #   echo "$(date +"%m/%d/%Y %T")-> PG Running pods Result: $POSTGRES - Attempt: $CNT" >> "${LOGFILE}" 
   #   if [ $CNT -eq 5 ]; then
-  #     echo "$(date +%F" "%T)-> Rebooting after 5 minutes with Postgres only $POSTGRES" >> "${LOGFILE}"
+  #     echo "$(date +"%m/%d/%Y %T")-> Rebooting after 5 minutes with Postgres only $POSTGRES" >> "${LOGFILE}"
   #     #sshpass -f /home/holuser/creds.txt ssh vmware-system-user@10.1.1.71 "sudo -i bash -c reboot"
   #     # Instead of a full reboot try deleting the vcfapostgres-0 pods, this forces the ReplicaSet to re-create them:
   #     sshpass -f /home/holuser/creds.txt ssh vmware-system-user@10.1.1.71 "sudo -i bash -c 'kubectl -n prelude delete pods vcfapostgres-0'"
@@ -114,9 +114,9 @@ echo "$(date +%F" "%T)-> VCFA Watcher started"  >> "${LOGFILE}"
   #   sleep 300;
 	#   CCSK3SAPP=$(sshpass -f /home/holuser/creds.txt ssh vmware-system-user@10.1.1.71 "sudo -i bash -c 'kubectl get pods -n prelude -s https://10.1.1.71:6443'" | grep ccs-k3s-app | awk '{ print $2 }')
 	#   ((CNT++))
-  #   echo "$(date +%F" "%T)-> CCS Running pods Result: $CCSK3SAPP - Attempt: $CNT" >> "${LOGFILE}" 
+  #   echo "$(date +"%m/%d/%Y %T")-> CCS Running pods Result: $CCSK3SAPP - Attempt: $CNT" >> "${LOGFILE}" 
   #   if [ $CNT -eq 12 ]; then
-  #     echo "$(date +%F" "%T)-> Rebooting after 60 minutes with CCS-K3SAPP only $POSTGRES" >> "${LOGFILE}"
+  #     echo "$(date +"%m/%d/%Y %T")-> Rebooting after 60 minutes with CCS-K3SAPP only $POSTGRES" >> "${LOGFILE}"
   #     sshpass -f /home/holuser/creds.txt ssh vmware-system-user@10.1.1.71 "sudo -i bash -c reboot"
   #     sleep 30
   #     break
@@ -126,11 +126,11 @@ echo "$(date +%F" "%T)-> VCFA Watcher started"  >> "${LOGFILE}"
   # if [ "$CCSK3SAPP" == "2/2" ]; then
   #   CCSK3SAPPNAME=$(sshpass -f /home/holuser/creds.txt ssh vmware-system-user@10.1.1.71 "sudo -i bash -c 'kubectl get pods -n prelude -s https://10.1.1.71:6443'" | grep ccs-k3s-app | awk '{ print $1 }')
   #   sshpass -f /home/holuser/creds.txt ssh vmware-system-user@10.1.1.71 "sudo -i bash -c 'kubectl delete pod '\"$CCSK3SAPPNAME\"' -n prelude -s https://10.1.1.71:6443'"
-  #   echo "$(date +%F" "%T)-> Deleted CCS-K3S-APP for CPU usage bug" >> "${LOGFILE}" 
+  #   echo "$(date +"%m/%d/%Y %T")-> Deleted CCS-K3S-APP for CPU usage bug" >> "${LOGFILE}" 
   #   break
   # fi
   # if [ "$POSTGRES" == "2/2" ]; then
-  #   echo "$(date +%F" "%T)-> Postgres is up, waiting for CCS-K3S-APP" >> "${LOGFILE}" 
+  #   echo "$(date +"%m/%d/%Y %T")-> Postgres is up, waiting for CCS-K3S-APP" >> "${LOGFILE}" 
   #   break
   # fi
 #   ((REBOOTS++))
