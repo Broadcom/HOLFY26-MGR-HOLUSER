dbd26c4850d83affe55383e6b7c2b83f844bc0f3
Burke Azbill <burkeazbill-bc@users.noreply.github.com>
2026-03-27 09:31:10 -0400

    Update to ignore vCLS vms when checking guest settings

--- Tools/vpodchecker.py
+++ Tools/vpodchecker.py
@@ -450,6 +450,7 @@ def check_vm_configuration(vms: List, fix_issues: bool = True) -> List[CheckResu
         'vcf-services-platform-template-',    # VCF Services Platform Template VMs
         'SupervisorControlPlaneVM',           # Tanzu Supervisor Control Plane VMs
         'vna-wld01-',                         # VNA Workload VMs
+        'vCLS-',                              # vCLS VMs
     ]
     
     for vm in vms:
